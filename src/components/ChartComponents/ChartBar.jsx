ae1b71cee96e90e5b4538ff92d5e20a621771aaa
Garrett <garrettswink421@gmail.com>
2023-11-05 15:00:41 -0500

    Chart Components

---
+++ src/components/ChartComponents/ChartBar.jsx
@@ -0,0 +1,18 @@
+import "./CharBar.css"
+
+export function ChartBar(props) {
+    let fillHeight = "0%";
+
+    if (props.maxValue > 0) {
+        fillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+    }
+
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar-inner">
+                <div className="chart-bar-fill" style={{height: fillHeight}}></div>
+            </div>
+            <div className="chart-bar-label">{props.label}</div>
+        </div>
+    )
+}
