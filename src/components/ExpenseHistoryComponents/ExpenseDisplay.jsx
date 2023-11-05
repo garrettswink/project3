15c3833eb81f842e00f3ead7483466d883d79a58
Garrett <garrettswink421@gmail.com>
2023-11-05 13:26:13 -0500

    Expense Filter; Expense Display

---
+++ src/components/ExpenseHistoryComponents/ExpenseDisplay.jsx
@@ -0,0 +1,12 @@
+// React Import
+import {useState} from "react";
+
+// File Import
+import './ExpenseDisplay.css'
+
+// Component Imports
+import {Card} from "../CardComponents/Card"
+// List
+// Chart
+// Filter 
+
---
+++ src/components/ExpenseHistoryComponents/ExpenseFilter.jsx
@@ -0,0 +1,22 @@
+import "./ExpenseFilter.css"
+
+export function ExpenseFilter(props) {
+
+    const dateChangeHandler = (e) => {
+        props.onChangeDate(e.target.value);
+    };
+
+    return (
+        <div className='expense-filter'>
+            <div className='expense-filter-control'>
+                <label>Filter by year</label>
+                <select value={props.default} onChange={dateChangeHandler}>
+                    <option value='2023'>2023</option>
+                    <option value='2022'>2022</option>
+                    <option value='2021'>2021</option>
+                    <option value='2020'>2020</option>
+                </select>
+            </div>
+        </div>
+    );
+}
